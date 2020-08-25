8dcfca51d10ed851fafc6e4072e55812b2a0a5bc
MN7 <narayan.m@gmail.com>
2020-08-25 21:35:48 +1000

    data displays in table now.

--- client/libviewer/src/kohajson/Viewinfo.js
+++ client/libviewer/src/kohajson/Viewinfo.js
@@ -13,6 +13,7 @@ class Viewinfo extends Component {
     super(props);
     this.state = {
       loading: false,
+      libdata: [],
       loaded: false
     };
   }
@@ -38,30 +39,31 @@ console.log("response received: "+res);
 
   parseKohaJSON = (rawJSON) => {
     console.log("typeof rawJSON: "+typeof rawJSON);
-    const temp =
-    rawJSON.map((x) => {
-      console.log(typeof x+" "+JSON.stringify(x));
-      return x.title;
-    });
-    this.setState({tmp: temp});
-alert("exit parseKohaJSON.. tiles: "+temp);
+    const temp = [...rawJSON.map((x) => {
+        const book=this.createData(x.biblionumber, x.title, x.author, x.copyrightdate, x.type);
+        return book;
+        // return { x.biblionumber, x.title, x.author, x.copyrightdate, x.type };
+        // return this.createData(x.title, x.author, x.copyrightdate, x.type);
+      })
+    ];
+    this.setState({libdata: temp, loaded: true});
+alert("exit parseKohaJSON..");
   }
 
-  createData = (name, calories, fat, carbs, protein) => {
-    return { name, calories, fat, carbs, protein };
+  createData = (id, title, author, year, type) => {
+    return { id, title, author, year, type };
   }
 
   render() {
 
-    const createData = this.createData;
-
+    // const dataLoaded = this.loaded;
     const rows = [
-      createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-      createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-      createData('Eclair', 262, 16.0, 24, 6.0),
-      createData('Cupcake', 305, 3.7, 67, 4.3),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      ...this.state.libdata
     ];
+
+    // const createData = this.createData;
+    // createData( 111, 'Frozen yoghurt', "abcd", 1924, "Paperback"),
+    // createData( 111, 'Test book 002', "pqrs", 2014, "Kindle"),
 
     //       <Table className={classes.table} aria-label="simple table">
 
@@ -70,34 +72,37 @@ alert("exit parseKohaJSON.. tiles: "+temp);
         <div className="vi-header">
           <h2>View JSON info from Koha</h2>
         </div>
-        <div className="vi-intro">
-          <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Dessert (100g serving)</TableCell>
-                      <TableCell align="right">Calories</TableCell>
-                      <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                      <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                      <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {rows.map((row) => (
-                      <TableRow key={row.name}>
-                        <TableCell component="th" scope="row">
-                          {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
+        { this.state.loaded ?
+          <div className="vi-intro">
+            <TableContainer component={Paper}>
+                  <Table aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Title</TableCell>
+                        <TableCell>Author</TableCell>
+                        <TableCell align="right">Year</TableCell>
+                        <TableCell>Type</TableCell>
                       </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-        </div>
+                    </TableHead>
+                    <TableBody>
+                      {rows.map((row) => (
+                        <TableRow key={row.id}>
+                          <TableCell component="th" scope="row">
+                            {row.title}
+                          </TableCell>
+                          <TableCell>{row.author}</TableCell>
+                          <TableCell align="right">{row.year}</TableCell>
+                          <TableCell>{row.type}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+          </div>
+          :
+          <div> <p> Data is being loaded from Koha JSON. Please wait. </p> </div>
+        }
+
       </div>
     );
   }
