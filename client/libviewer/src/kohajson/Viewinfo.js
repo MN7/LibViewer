8bc0a213b053b6e2e2a195c16bea07e7dd4d1bb1
MN7 <narayan.m@gmail.com>
2020-08-25 18:55:28 +1000

    adding material-core

--- client/libviewer/src/kohajson/Viewinfo.js
+++ client/libviewer/src/kohajson/Viewinfo.js
@@ -1,14 +1,69 @@
 import React, { Component } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 class Viewinfo extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = [];
+  }
+
+  createData = (name, calories, fat, carbs, protein) => {
+    return { name, calories, fat, carbs, protein };
+  }
+
   render() {
+
+    const createData = this.createData;
+
+    const rows = [
+      createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+      createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+      createData('Eclair', 262, 16.0, 24, 6.0),
+      createData('Cupcake', 305, 3.7, 67, 4.3),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+    ];
+
+    //       <Table className={classes.table} aria-label="simple table">
+
     return (
       <div className="viewinfo">
         <div className="vi-header">
           <h2>View JSON info from Koha</h2>
         </div>
         <p className="vi-intro">
-          To get started, edit <code>src/kohajson/Viewinfo.js</code> and save to reload.
+          <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Dessert (100g serving)</TableCell>
+                      <TableCell align="right">Calories</TableCell>
+                      <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                      <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                      <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow key={row.name}>
+                        <TableCell component="th" scope="row">
+                          {row.name}
+                        </TableCell>
+                        <TableCell align="right">{row.calories}</TableCell>
+                        <TableCell align="right">{row.fat}</TableCell>
+                        <TableCell align="right">{row.carbs}</TableCell>
+                        <TableCell align="right">{row.protein}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
         </p>
       </div>
     );
